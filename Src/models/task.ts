bb84c245413654def26716f1eaf819759e4ca44e
Aeiyan Khan <110762239+aeiyannn@users.noreply.github.com>
2024-01-14 20:23:02 +0500

    update(*)

--- Src/models/task.ts
+++ Src/models/task.ts
@@ -1,23 +1,91 @@
-"use strict";
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../database/index';
+import User from './user';
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.addColumn("tasks", "priority", {
-            type: Sequelize.DataTypes.ENUM(
+interface TaskAttributes {
+    id: number;
+    taskTitle: string;
+    taskDescription: string;
+    dueDate: string
+    assignBy: number;
+    assignTo: number;
+    taskStatus: string;
+    priority: string
+}
+
+class Task extends Model<TaskAttributes> implements TaskAttributes {
+    public id!: number;
+    public taskTitle!: string;
+    public taskDescription!: string;
+    public dueDate!: string;
+    public assignBy!: number;
+    public assignTo!: number;
+    public taskStatus!: string;
+    public priority!: string
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+    public sender?: User;
+    public userInfo?: User;
+
+}
+
+Task.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        taskTitle: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        taskDescription: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dueDate: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        assignBy: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        assignTo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        taskStatus: {
+            type: DataTypes.ENUM("pending", "started", "onprogress", "completed", "notcomplete"),
+            allowNull: false,
+            defaultValue: 'pending'
+        },
+        priority: {
+            type: DataTypes.ENUM(
                 "pending",
                 "started",
                 "onprogress",
                 "complete",
                 "notcomplete"
             ),
-            allowNull: false, // Change allowNull to false if needed
-            defaultValue: "pending",
-        });
+            allowNull: false,
+        }
     },
+    {
+        sequelize,
+        modelName: 'Task',
+        tableName: 'tasks', // Define the table name explicitly
+    }
+);
+Task.belongsTo(User, {
+    foreignKey: 'assignBy',
+    as: 'sender',
+});
+Task.belongsTo(User, {
+    foreignKey: 'assignTo',
+    as: 'userInfo',
+});
 
-    async down(queryInterface, Sequelize) {
-        // Remove the column in case of a rollback
-        await queryInterface.removeColumn("tasks", "priority");
-    },
-};
+export default Task;
