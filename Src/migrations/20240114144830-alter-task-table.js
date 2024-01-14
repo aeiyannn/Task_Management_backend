7717f0b49e6f549c6471e22ba1885bdbdc61c32c
Aeiyan Khan <110762239+aeiyannn@users.noreply.github.com>
2024-01-14 20:21:48 +0500

    update(*)

---
+++ Src/migrations/20240114144830-alter-task-table.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("tasks", "priority", {
+      type: Sequelize.DataTypes.ENUM(
+        "pending",
+        "started",
+        "onprogress",
+        "complete",
+        "notcomplete"
+      ),
+      allowNull: false, // Change allowNull to false if needed
+      defaultValue: "pending",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    // Remove the column in case of a rollback
+    await queryInterface.removeColumn("tasks", "priority");
+  },
+};
