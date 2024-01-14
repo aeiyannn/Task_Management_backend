ec0c9d6399787bca1c02f161e7b6534ddb1e2304
Aeiyan Khan <110762239+aeiyannn@users.noreply.github.com>
2024-01-14 15:26:54 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import taskRoute from './Src/route/task';
 import './Src/database/index';
 import cors from "cors"
 const app = express();
-const port = 2000;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
